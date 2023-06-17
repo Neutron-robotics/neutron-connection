2d1d48adc675ed8a01c6812dd3602c245a601478
hugo <hugo.perier@cern.ch>
2023-06-17 23:32:17 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use std::io;
+extern crate dotenv;
+// use database::redis::get_connection;
+use dotenv::dotenv;
+use utils::args::Args;
+
+pub mod database;
+pub mod utils;
+
+use clap::Parser;
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
 
-    let mut guess = String::new();
+    let args = Args::parse();
+    utils::args::print_args(args);
 
-    io::stdin().read_line(&mut guess).expect("Fails to read new line");
-    
-    println!("You guessed: {guess}");
+    // let mut conn = get_connection();
+
+    // let _: () = redis::cmd("SET")
+    //     .arg("foo")
+    //     .arg("bar")
+    //     .query(&mut conn)
+    //     .expect("failed to execute SET for 'foo'");
+
+    // let bar: String = redis::cmd("GET")
+    //     .arg("foo")
+    //     .query(&mut conn)
+    //     .expect("failed to execute GET for 'foo'");
+
+    // println!("value for 'foo' = {}", bar);
 }
---
+++ src/utils/args.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about="The official Neutron websocket connection server that is in charge of transporting and managing network interactions for individual robot connections", long_about = None)]
+pub struct Args {
+    #[clap(long)]
+    /// The id of the connection
+    pub id: String,
+
+    #[clap(long, short, short = 'm')]
+    /// The master id for the connection
+    pub connection_master: String,
+
+    #[clap(long, short, short = 'r')]
+    /// The robot id for the connection
+    pub robot_id: String,
+
+    #[clap(long, short, short = 'c')]
+    /// The robot id for the connection
+    pub redis_connection_string: String,
+}
+
+pub fn print_args(args: Args) {
+    println!("Neutron Connection started!");
+    println!("Args:");
+    println!("id: {}", args.id);
+    println!("connection_master: {}", args.connection_master);
+    println!("robot_id: {}", args.robot_id);
+    println!("redis_connection_string: {}", args.redis_connection_string);
+}
